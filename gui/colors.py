946ab74808367fd6e29aba74005f65807886719c
최원호 <wonho9188@gmail.com>
2025-06-10 21:47:45 +0900

    [gui] login page setting

---
+++ gui/colors.py
@@ -0,0 +1,8 @@
+colors = {
+    "primary" : "#03B4C8",
+    "primary_dark" : "#0E9FAF",
+    "secondary" : "#345984",
+    "therity" : "#F98CAB",
+    "fourth" : "#F2CD3B",
+    "fourth_dark" : "#D7B017",
+}
