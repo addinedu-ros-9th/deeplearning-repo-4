55d000455b042a5c45c4310d7dc50b2bde4bcb4e
최원호 <wonho9188@gmail.com>
2025-06-11 20:31:38 +0900

    [gui] qss setting

--- gui/login.py
+++ gui/login.py
@@ -14,6 +14,17 @@ class Main(QMainWindow):
         self.setWindowTitle("Gigachad | Login")
         # .ui 파일 로드
         loadUi("login.ui", self)
+        self.login_btn.setProperty("class", "btn contained primary large")
+        self.login_checkbox.setProperty("class", "checkbox gray9")
+        self.login_div.setProperty("class", "divider ver dotted")
+        self.login_div2.setProperty("class", "divider ver dotted")
+        self.login_textfield1.setProperty("class", "textfield large")
+        self.login_textfield2.setProperty("class", "textfield large")
+        self.login_greeting.setProperty("class", "size24 weight700")
+        self.login_desc1.setProperty("class", "size16 weight300 color-gray9")
+        self.login_desc2.setProperty("class", "size16 weight300 color-gray9")
+        self.login_id.setProperty("class", "size14 color-gray3")
+        self.login_password.setProperty("class", "size14 color-gray3")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- gui/style.py
+++ gui/style.py
@@ -10,15 +10,24 @@ colors = {
     "fourth_dark" : "#D7B017",
 }
 
+radius = "2px"
+
 def apply_style(app):
     qss_path = "./style/qss/"
-    qss_files = ["layout.qss", "login.qss"]
+    qss_files = ["layout.qss", 
+                 "login.qss",
+                 "utils.qss",
+                 "component/btn.qss",
+                 "component/checkbox.qss",
+                 "component/divider.qss",
+                 "component/textfield.qss"]
     style = ""
     for file in qss_files:
         with open(f'{qss_path}{file}', "r") as f:
             style += f.read() + "\n"
     for key, value in colors.items():
         style = style.replace("{{" + key + "}}", value)
+    style = style.replace("{{radius}}", radius)
     app.setStyleSheet(style)
 
     # # 폰트 설정 # 폰트 없는게 더 나은 거 같기도 해서 일단 빼놓겠습니다.
